7c917095b26d8eb6f33e9345239318bf4ebc128d
Andrea Collamati <andrea.collamati@elettronicagf.it>
2025-01-30 14:13:15 +0100

    3sm1008: quiet boot

--- include/configs/egf_3sm1008.h
+++ include/configs/egf_3sm1008.h
@@ -130,7 +130,7 @@
 	"bootm_size=0x10000000\0" \
 	"mmcdev=2\0" \
 	"mmcpart=1\0" \
-	"mmcroot=/dev/mmcblk2p2 rootwait rw\0" \
+	"mmcroot=/dev/mmcblk2p2 rootwait rw quiet \0" \
 	"mmcautodetect=yes\0" \
 	"mmcargs=setenv bootargs ${jh_clk} ${mcore_clk} console=${console} root=${mmcroot}\0 " \
 	"loadbootscript=fatload mmc ${mmcdev}:${mmcpart} ${loadaddr} ${bsp_script};\0" \
